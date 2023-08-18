892b3ba8f69d0bc5a75e80af216070f8e4cf5798
Ryan Deyo <rdeyo1129@gmail.com>
2023-08-18 12:14:51 -0400

    fs path thunk sass, rm saga, fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import fs from "fs/promises";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  // todo
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    // port: process.env.PORT || 8080,
+    resolve: {
+      alias: {
+        src: resolve(__dirname, "src"),
+      },
+    },
+    esbuild: {
+      loader: "tsx",
+      include: /src\/.*\.tsx?$/,
+      exclude: [],
+    },
+    build: {
+      outDir: "dist",
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        plugins: [
+          {
+            name: "load-ts-files-as-tsx",
+            setup(build) {
+              build.onLoad({ filter: /src\\.*\.ts$/ }, async (args) => ({
+                loader: "tsx",
+                contents: await fs.readFile(args.path, "utf8"),
+              }));
+            },
+          },
+        ],
+      },
+    },
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: `http://localhost:${process.env.PORT || 8080}`,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  });
+};

c69af05d1ebdbc806c8f55ec05009ddf901efcbb
Ryan Deyo <rdeyo1129@gmail.com>
2023-11-01 23:44:50 -0400

    start book and json structure

--- src/pages/book/Book.tsx
+++ src/pages/book/Book.tsx
@@ -9,33 +9,156 @@ import 'src/chessground/styles/normal.scss';
 
 // import 'src/pages/inGameMenu/InGameMenu.scss';
 
-import { setLocalStorage, getLocalStorage } from 'src/utils/handleLocalStorage';
-
 import { Chessground } from 'src/chessground/chessgroundMod';
 
 import Button from 'src/components/Button/Button';
 import Spinner from 'src/components/Loader/Spinner';
 import Dots from 'src/components/Loader/Dots';
 
+import { setLocalStorage, getLocalStorage } from 'src/utils/handleLocalStorage';
+/*
+  // // DYNAMIC IMPORTS \\ \\ THIS RUNS INTO AN ISSUE, JUST IMPORT THEM ALL
+*/
+
+// import instead, use ex here
+const jsonChpater1 = { a: 1 };
+const jsonChapter2 = { a: 2, inbox: ['test', 'test2'] };
+
 interface BookProps {
   auth: { [key: string]: any };
 }
 interface BookState {
-  // config: { [key: string]: any };
+  [key: string]: any;
+}
+
+interface lessonNode {
+  id: string; // 'lesson-1';
+  title: string;
+  time: [number, number]; // seconds
+  chatLog: [string | null, string | null];
+  reward: (number | string[])[];
+  boards: {
+    [key: string]: {
+      fen: string;
+      fenHistory: string[];
+      history: string[];
+      // is arrows this a map?
+      arrows?: string[][];
+      // todo initRoyalties in arcaneChess return object
+      royalties: {
+        royaltyQ: string[];
+        royaltyM: string[];
+        royaltyT: string[];
+        royaltyV: string[];
+        royaltyE: string[];
+      };
+      whiteArcane?: { [key: string]: number };
+      blackArcane?: { [key: string]: number };
+      // orientation: string;
+      config: {
+        [key: string]: boolean | string | number;
+      };
+      dialogue: [
+        // [ 'narrator', 'message']
+        // [ 'medavas', 'message']
+        // no text from creator, just put in a blank message that doesn't add anything to the ui
+        [string | null, string | null],
+      ];
+    };
+  };
+}
+
+const templeNode = {};
+
+// JSON STRUCTURE
+// todo to be generated / automated from creator
+interface missionNode {
+  id: string; // 'mission-1';
+  title: string;
+  time: [number, number]; // seconds
+  chatLog: [string | null, string | null];
+  fen: string;
+  fenHistory: string[];
+  history: string[];
+  // is arrows this a map?
+  arrows?: string[][];
+  // todo initRoyalties in arcaneChess return object
+  royalties: {
+    royaltyQ: string[];
+    royaltyM: string[];
+    royaltyT: string[];
+    royaltyV: string[];
+    royaltyE: string[];
+  };
+  whiteArcane?: { [key: string]: number };
+  blackArcane?: { [key: string]: number };
+  orientation: string;
+  handicaps: {
+    [key: string]: boolean | string | number;
+  };
+  dialogue: [
+    // [ 'narrator', 'message']
+    // [ 'medavas', 'message']
+    // no text from creator, just put in a blank message that doesn't add anything to the ui
+    [string | null, string | null],
+  ];
 }
 
 export class UnwrappedBook extends React.Component<BookProps, BookState> {
-  // what type is props supposed to be
   constructor(props: BookProps) {
     super(props);
-    this.state = {};
+    this.state = {
+      // get all nodes from json import and .map in render
+      // to conditionally render right side of view depending on current node id
+      pointsEx: 3552,
+      inboxEx: ['mission-1', 'mission-2', 'mission-3', 'mission-4'],
+      dialogueEx: [
+        ['sidian', 'message'],
+        ['narrator', 'message'],
+        ['hero', 'message'],
+      ],
+      chapter: [
+        `jsonChapter${getLocalStorage(this.props.auth.user.id).chapter}`,
+      ],
+      config: getLocalStorage(this.props.auth.user.id).config,
+      nodeScores: getLocalStorage(this.props.auth.user.id).nodeScores,
+      inventory: getLocalStorage(this.props.auth.user.id).inventory,
+    };
   }
   render() {
+    console.log(this.state.chapter);
     return (
       <div className="book">
         <div className="top">
-          <div className="chapters">chapters</div>
-          <div className="messages">messages</div>
+          <div className="inbox">
+            {this.state.inboxEx.map((mission: string, i: number) => {
+              return (
+                <div key={i} className="swatch">
+                  <div className="title">{mission}</div>
+                  <div className="time">time</div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="messages">
+            {this.state.dialogueEx.map((message: string[], i: number) => {
+              return (
+                <div
+                  key={i}
+                  className={`message${
+                    message[0] === 'narrator'
+                      ? '-narrator'
+                      : message[0] === 'hero'
+                      ? '-hero'
+                      : ''
+                  }`}
+                >
+                  <div className="name">{message[0]}</div>
+                  <div className="text">{message[1]}</div>
+                </div>
+              );
+            })}
+          </div>
           {/* <div className="board-view"> */}
           <div className="cg-wrap tactorius-board">
             <Chessground
@@ -124,8 +247,41 @@ export class UnwrappedBook extends React.Component<BookProps, BookState> {
           <div className="arcane-time">arcane-time</div>
         </div>
         <div className="bottom">
-          {/* <div className="left"></div>
-          <div className="right"></div> */}
+          <div className="left">
+            <Button
+              text="BACK"
+              className="secondary"
+              color="V"
+              width={120}
+              height={40}
+              disabled={false}
+            />
+            <Button
+              text="ARMORY"
+              className="secondary"
+              color="V"
+              width={120}
+              height={40}
+              disabled={false}
+            />
+          </div>
+          <div className="center">
+            <span>
+              {/* {this.state.inventory.points} */}
+              {this.state.pointsEx} X {this.state.config.multiplier} ={' '}
+              {this.state.pointsEx * this.state.config.multiplier} kudos
+            </span>
+          </div>
+          <div className="right">
+            <Button
+              text="START"
+              className="primary"
+              color="V"
+              width={120}
+              height={40}
+              disabled={false}
+            />
+          </div>
         </div>
       </div>
     );

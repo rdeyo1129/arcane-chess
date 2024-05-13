e6e8030649906a71661fa3b3d9847a930914f6c0
Ryan Deyo <rdeyo1129@gmail.com>
2024-05-13 13:36:42 -0400

    server file clean comments shuffle imports serve index html order

--- server/server.ts
+++ server/server.ts
@@ -1,27 +1,25 @@
-// test 27488a30-1ff3-4f9a-b127-b63f68aabf76
-
-// import _ from 'lodash';
 import bodyParser from 'body-parser';
 import fs from 'fs';
-
-// import passport from "passport";
-// import passportConfig from "./config/passport.mjs";
-
-// import { v4 } from 'uuid';
-
-// Load Game and User model
-// import { Game } from "./src/models/Game.mjs";
-// import User from "./src/models/User.mjs";
-
-// import { keys } from './config/keys.js';
-
-// { ConnectOptions } from "mongoose";
 import mongoose from 'mongoose';
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 import dotenv from 'dotenv';
+
+import users from './api/users.js';
+import games from './api/games.js';
+import campaign from './api/campaign.js';
+import templates from './api/templates.js';
+import puzzles from './api/puzzles.js';
+
+import express from 'express';
+import crypto from 'crypto';
+import path from 'path';
+import { createServer } from 'http';
+// import { Server } from 'socket.io';
+
+import favicon from 'serve-favicon';
 
 const nodeEnv = process.env.NODE_ENV || 'development';
 const envPath = path.resolve(process.cwd(), `.env.${nodeEnv}`);
@@ -40,35 +38,9 @@ mongoose
   .then(() => console.log('MongoDB successfully connected'))
   .catch((err) => console.log(err, 'mongo uri:', process.env.MONGO_URI));
 
-import users from './api/users.js';
-import games from './api/games.js';
-import campaign from './api/campaign.js';
-import templates from './api/templates.js';
-import puzzles from './api/puzzles.js';
-
-import express from 'express';
-import crypto from 'crypto';
-import path from 'path';
-import { createServer } from 'http';
-// import { Server } from 'socket.io';
-
-import favicon from 'serve-favicon';
-
 const app = express();
 const server = createServer(app);
 // const io = new Server(server);
-
-const port = process.env.PORT || 8080;
-
-if (process.env.NODE_ENV === 'production') {
-  console.log(
-    'Serving CSS from:',
-    path.join(__dirname, '..', '..', 'static', 'main.css')
-  );
-  const staticPath = path.join(__dirname, '..', '..', 'static');
-  app.use(express.static(staticPath));
-  app.use(favicon(path.join(__dirname, '..', '..', 'favicon.ico')));
-}
 
 // use body parser
 app.use(
@@ -78,16 +50,20 @@ app.use(
 );
 app.use(bodyParser.json());
 
+const port = process.env.PORT || 8080;
+
+const staticPath = path.join(__dirname, 'dist', 'frontend');
+app.use(express.static(staticPath));
+if (process.env.NODE_ENV === 'production') {
+  app.use(favicon(path.join(staticPath, 'favicon.ico')));
+}
+
 // Routes
 app.use('/api/users', users);
 app.use('/api/games', games);
 app.use('/api/campaign', campaign);
 app.use('/api/templates', templates);
 app.use('/api/puzzles', puzzles);
-
-const frontendPath = path.join(__dirname, 'dist', 'frontend');
-
-app.use(express.static(frontendPath));
 
 // Helper to generate nonce
 const generateNonce = () => crypto.randomBytes(16).toString('base64');
@@ -97,9 +73,6 @@ app.use((_req, res, next) => {
   res.locals.nonce = generateNonce();
   next();
 });
-
-// Serve static files from the correct directory
-app.use(express.static('dist/frontend'));
 
 // Serve HTML with nonce
 app.get('*', (_req, res) => {

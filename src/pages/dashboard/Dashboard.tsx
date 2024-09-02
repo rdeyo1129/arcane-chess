0c1690168a2348a650bb5ffda62885b0c5daf180
Ryan Deyo <rdeyo1129@gmail.com>
2024-09-01 23:55:04 -0400

    image updates, add pages, dashboard update

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -128,62 +128,78 @@ export class UnwrappedDashboard extends React.Component<
               // strong={true}
             />
           </Link>
-          {process.env.NODE_ENV !== 'production' ? (
-            <Link
-              className="home-button"
-              to="/about"
-              onMouseEnter={() => this.setState({ hoverNav: 'create' })}
-            >
-              <Button
-                text="ABOUT"
-                onClick={() => null}
-                className="tertiary"
-                color="B"
-                height={50}
-                width={200}
-                // disabled={this.state.fen === ''}
-                disabled={false}
-                backgroundColorOverride="#11111188"
-                // strong={true}
-              />
-            </Link>
-          ) : (
-            <Link
-              className="home-button"
-              to="/create"
-              onMouseEnter={() => this.setState({ hoverNav: 'create' })}
-            >
-              <Button
-                text="CREATE"
-                onClick={() => null}
-                className="tertiary"
-                color="B"
-                height={50}
-                width={200}
-                // disabled={this.state.fen === ''}
-                disabled={false}
-                backgroundColorOverride="#11111188"
-                // strong={true}
-              />
-            </Link>
-          )}
-
-          <Button
-            text="LOGOUT"
-            // onClick={() => this.calculateFen()}
-            className="tertiary"
-            color="B"
-            height={50}
-            width={200}
-            // disabled={this.state.fen === ''}
-            disabled={false}
-            onClick={() => {
-              this.props.logoutUser();
-              this.props.navigate('/login');
-            }}
-            backgroundColorOverride="#11111188"
-            // strong={true}
-          />
+          {/* <Link
+               className="home-button"
+               to="/create"
+               onMouseEnter={() => this.setState({ hoverNav: 'create' })}
+             >
+               <Button
+                 text="CREATE"
+                 onClick={() => null}
+                 className="tertiary"
+                 color="B"
+                 height={50}
+                 width={200}
+                 // disabled={this.state.fen === ''}
+                 disabled={false}
+                 backgroundColorOverride="#11111188"
+                 // strong={true}
+               />
+             </Link> */}
+          <Link
+            className="home-button"
+            to="/changelog"
+            onMouseEnter={() => this.setState({ hoverNav: 'create' })}
+          >
+            <Button
+              text="CHANGELOG"
+              onClick={() => null}
+              className="tertiary"
+              color="B"
+              height={50}
+              width={200}
+              // disabled={this.state.fen === ''}
+              disabled={false}
+              backgroundColorOverride="#11111188"
+              // strong={true}
+            />
+          </Link>
+          <Link
+            className="home-button"
+            to="/manifest"
+            onMouseEnter={() => this.setState({ hoverNav: 'manifest' })}
+          >
+            <Button
+              text="MANIFEST"
+              onClick={() => null}
+              className="tertiary"
+              color="B"
+              height={50}
+              width={200}
+              // disabled={this.state.fen === ''}
+              disabled={false}
+              backgroundColorOverride="#11111188"
+              // strong={true}
+            />
+          </Link>
+          <div onMouseEnter={() => this.setState({ hoverNav: 'logout' })}>
+            <Button
+              text="LOGOUT"
+              // onClick={() => this.calculateFen()}
+              className="tertiary"
+              color="B"
+              height={50}
+              width={200}
+              // disabled={this.state.fen === ''}
+              disabled={false}
+              onClick={() => {
+                this.props.logoutUser();
+                this.props.navigate('/login');
+              }}
+              backgroundColorOverride="#11111188"
+              // strong={true}
+            />
+          </div>
         </div>
         <div className="hover-image">
           <img

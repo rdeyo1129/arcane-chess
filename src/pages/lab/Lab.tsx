ff7d79c999bd3ead94a46550fb1320e705a8e8f6
Ryan Deyo <rdeyo1129@gmail.com>
2025-02-11 23:33:33 -0500

    book 12 content, images, story, dialogue, prereqs, board labelings

--- src/pages/lab/Lab.tsx
+++ src/pages/lab/Lab.tsx
@@ -6,16 +6,16 @@ import _ from 'lodash';
 // import book2 from 'src/data/books/book2.json';
 // import book3 from 'src/data/books/book3.json';
 // import book4 from 'src/data/books/book4.json';
-import book5 from 'src/data/books/book5.json';
+// import book5 from 'src/data/books/book5.json';
 // import book6 from 'src/data/books/book6.json';
 // import book7 from 'src/data/books/book7.json';
 // import book8 from 'src/data/books/book8.json';
 // import book9 from 'src/data/books/book9.json';
 // import book10 from 'src/data/books/book10.json';
 // import book11 from 'src/data/books/book11.json';
-// import book12 from 'src/data/books/book12.json';
+import book12 from 'src/data/books/book12.json';
 
-const microscopeData: any = book5;
+const microscopeData: any = book12;
 
 interface CustomTreeNodeDatum {
   name: string;
